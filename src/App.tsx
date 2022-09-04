ce2ad0120ca53c30921d5edf4b40b856894b588d
kalz2q <katsu95i@gmail.com>
2022-09-04 10:52:18 +0900

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+type TodoItem = {
+  id: number;
+  content: string;
+};
 
 function App() {
+  useEffect(() => {
+    document.title = "react-list-map-samples";
+  });
+
+  const todos: TodoItem[] = [
+    { id: 1, content: "do something" },
+    { id: 2, content: "go somewhere" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <ol className="todo-list">
+        {todos.map((todo: TodoItem) => {
+          return (
+            <li className="todo-item" key={todo.id}>
+              <span className="todo-item__text">{todo.content}</span>
+            </li>
+          );
+        })}
+      </ol>
     </div>
   );
 }
